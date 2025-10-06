84abb5e09ef4ff8bf23be9c06db27ebb606669a1
AvengerAnubis <avenger.anubis@gmail.com>
2025-10-06 21:24:29 +0500

    Создание сервиса чата и класса сообщения

---
+++ ChatbotLib/ChatMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatbotLib
+{
+    public class ChatMessage
+    {
+        public string Author { get; set; } = "noname";
+        public string Message { get; set; } = string.Empty;
+    }
+}
--- ChatbotLib/ChatService.cs
+++ ChatbotLib/ChatService.cs
@@ -1,7 +1,79 @@
-﻿namespace ChatbotLib
-{
-    public class ChatService
-    {
+﻿using System.IO.Pipelines;
+using System.Reflection;
+using System.Text.Json;
 
+namespace ChatbotLib
+{
+    public class ChatService : IDisposable
+    {
+        public List<ChatMessage> Messages { get; protected set; } = [];
+
+        #region Отправка сообщений
+        public void SendMessage(ChatMessage message) => Messages.Add(message);
+        public void SendMessage(string author, string message) 
+            => SendMessage(new() { Author = author, Message = message });
+        #endregion
+        #region Serialization/Deserialization
+        protected JsonSerializerOptions options = new()
+        {
+            WriteIndented = false,
+            IncludeFields = false,
+            PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower
+        };
+        protected CancellationTokenSource sharedCts = new();
+        public async Task SaveToJson(CancellationToken token = default)
+        {
+            var registerToken = token.Register(() => sharedCts.Cancel());
+
+            using FileStream file = File.OpenWrite(Assembly.GetExecutingAssembly().Location + @"\chat.json");
+
+            await JsonSerializer.SerializeAsync(file, Messages, options, sharedCts.Token);
+
+            registerToken.Unregister();
+        }
+        public async Task LoadFromJson(CancellationToken token = default)
+        {
+            var registerToken = token.Register(() => sharedCts.Cancel());
+
+            try
+            {
+                using FileStream file = File.OpenRead(Assembly.GetExecutingAssembly().Location + @"\chat.json");
+                List<ChatMessage>? messages = await JsonSerializer.DeserializeAsync<List<ChatMessage>>(file, options, sharedCts.Token);
+                if (messages is not null) 
+                    Messages = messages;
+            }
+            catch (Exception ex) when (
+                ex is UnauthorizedAccessException ||
+                ex is FileNotFoundException ||
+                ex is JsonException
+            )
+            {
+                Messages.Clear();
+            }
+
+
+            registerToken.Unregister();
+        }
+        #endregion
+
+        #region Disposing
+        protected bool isDisposed = false;
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected void Dispose(bool disposing)
+        {
+            if (!isDisposed)
+            {
+                if (disposing)
+                {
+                    sharedCts.Cancel();
+                    sharedCts.Dispose();
+                }
+            }
+        }
+        #endregion
     }
 }
