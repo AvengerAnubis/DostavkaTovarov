0981f2a8270839a21f389fcd07270d8f2032a4cf
AvengerAnubis <avenger.anubis@gmail.com>
2025-10-06 23:37:07 +0500

    Добавлен класс Q&A иерархии

---
+++ ChatbotLib/QuestionAnswerHierarchyNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatbotLib
+{
+    public class QuestionAnswerHierarchyNode
+    {
+        public string Question { get; set; } = string.Empty;
+        public string Answer { get; set; } = string.Empty;
+        public List<QuestionAnswerHierarchyNode> Children { get; } = [];
+    }
+}
