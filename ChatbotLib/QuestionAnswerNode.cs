b7131ec5e8887cd22e74cbd05d5676bf190dcf7a
AvengerAnubis <avenger.anubis@gmail.com>
2025-10-07 06:15:21 +0500

    Переработка класса QANode, для работы с контекстом

---
+++ ChatbotLib/QuestionAnswerNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatbotLib
+{
+    public class QuestionAnswerNode
+    {
+        public string Question { get; set; } = string.Empty;
+        public string QuestionNormalized => Question.Trim().ToLower();
+        public string QuestionContexted { get; set; } = string.Empty;
+        public string QuestionContextedNormalized => QuestionContexted.Trim().ToLower();
+
+        public string Answer { get; set; } = string.Empty;
+        public IEnumerable<QuestionAnswerNode> ContextChildren { get; set; } = [];
+    }
+}
