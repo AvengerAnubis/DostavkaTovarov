8f67d4e70463614d2266ac7f035c7857f5249cde
AvengerAnubis <avenger.anubis@gmail.com>
2025-10-08 22:26:07 +0500

    Создана базовая структура

---
+++ ChatbotGui.QANEditor/App.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using ChatbotGui.QANEditor.Views;
+using ChatbotLib.Interfaces;
+using ChatbotLib.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ChatbotGui.QANEditor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static IHost? AppHost { get; private set; }
+
+        public App()
+        {
+            AppHost = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    // Сервисы
+                    services.AddSingleton<IDataSavingService, DataSavingService>();
+                    services.AddSingleton<IChatService, ChatService>();
+                    services.AddSingleton<IAnswerFinderService, AnswerFinderService>();
+
+                    // ViewModels
+
+
+                    // Страницы
+                    services.AddSingleton<DataViewerView>();
+                    services.AddKeyedTransient<NodeEditorView>();
+
+                    // Окна
+                    services.AddSingleton<MainWindow>();
+                })
+                .Build();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await AppHost!.StartAsync();
+
+            var mainWindow = AppHost.Services.GetRequiredService<MainWindow>();
+            MainWindow = mainWindow;
+            mainWindow.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await AppHost!.StopAsync();
+            AppHost.Dispose();
+            base.OnExit(e);
+        }
+    }
+
+}
---
+++ ChatbotGui.QANEditor/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using ChatbotGui.QANEditor.Views;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatbotGui.QANEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly IServiceProvider serviceProvider;
+        readonly DataViewerView dataViewerView;
+
+        public MainWindow(IServiceProvider serviceProvider, DataViewerView dataViewerView)
+        {
+            InitializeComponent();
+            this.serviceProvider = serviceProvider;
+            this.dataViewerView = dataViewerView;
+        }
+    }
+}
